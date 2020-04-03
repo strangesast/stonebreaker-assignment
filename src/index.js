0fda6ceae9ba932a10281a0e68aa2f79688908fc
Sam Zagrobelny <strangesast@gmail.com>
2020-04-03 18:02:26 -0400

    oops

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './index.scss';
 
+const form = document.querySelector('form');
+
 function textAreaAdjust() {
   this.style.height = '1px';
   this.style.height = `${25+this.scrollHeight}px`;
@@ -19,27 +21,44 @@ for (const el of document.body.querySelectorAll('input.inline')) {
   el.onkeyup = inlineInputAdjust;
 }
 
+function saveValue(value) {
+  localStorage.setItem('save', JSON.stringify(value));
+}
+
+function loadValueFromJSON(value) {
+  for (const el of form.elements) {
+    const name = el.getAttribute('name');
+    if (name in value) {
+      el.value = value[name];
+    }
+    if (el.tagName == 'TEXTAREA') {
+      textAreaAdjust.call(el);
+    }
+  }
+}
+
+function reset() {
+  for (const el of form.elements) {
+    el.value = '';
+    if (el.tagName == 'TEXTAREA') {
+      textAreaAdjust.call(el);
+    }
+  }
+}
+
+
 let value;
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('form');
   try {
     value = JSON.parse(localStorage.getItem('save'));
-    for (const el of form.elements) {
-      const name = el.getAttribute('name');
-      if (name in value) {
-        el.value = value[name];
-      }
-      if (el.tagName == 'TEXTAREA') {
-        textAreaAdjust.call(el);
-      }
-    }
+    loadValueFromJSON(value);
   } catch (e) {
     value = {};
   }
   form.oninput = e => {
     const el = e.srcElement;
     value[el.getAttribute('name')] = el.value;
-    localStorage.setItem('save', JSON.stringify(value));
+    saveValue(value);
   }
 });
 
@@ -60,3 +79,33 @@ document.getElementById('save-json').onclick = function (e) {
     document.body.removeChild(a);
   }, 100);
 }
+
+const fileInputElement = document.getElementById('file-input');
+document.getElementById('load-json').onclick = () => fileInputElement.click();
+
+fileInputElement.onchange = function () {
+  if (fileInputElement.files.length) {
+    const file = fileInputElement.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file, 'UTF-8');
+    reader.onload = function (e) {
+      try {
+        const value = JSON.parse(reader.result);
+        loadValueFromJSON(value);
+        saveValue(value);
+      } catch (e) {
+        alert('failed to read file');
+      }
+    }
+    reader.onerror = function () {
+      alert('failed to read file');
+    }
+  }
+}
+
+document.getElementById('clear').onclick = () => {
+  if (window.confirm('Are you sure?')) {
+    reset();
+    saveValue({});
+  }
+};
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,9 +1,11 @@
 const merge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = require('./webpack.config.base.js');
 
 module.exports = merge(baseConfig, {
+  plugins: [ new CopyPlugin([{from: 'static', to: 'static'}]) ],
   mode: 'production',
   optimization: {
     minimize: true,
